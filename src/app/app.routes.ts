355116cbb3913006f10b0e699368cda860e2a095
Mayar-ashraf <mayarashraf2002@gmail.com>
2025-08-05 02:52:15 +0300

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { ProductsListComponent } from './features/products/products-list/products-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'', redirectTo:'home', pathMatch:'full'},
+    {path:'home', loadComponent:()=>import('./features/products/products-list/products-list.component').then(c=>c.ProductsListComponent)}
+];
---
+++ src/app/features/products/products-list/products-list.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { ProductsService } from '../services/products.service';
+import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-products-list',
+  standalone: true,
+  imports: [HttpClientModule, CommonModule],
+  templateUrl: './products-list.component.html',
+  styleUrl: './products-list.component.scss',
+  providers:[ProductsService]
+})
+export class ProductsListComponent implements OnInit{
+  products: any[] = [];
+  constructor(private productsService: ProductsService){
+
+  }
+  ngOnInit(): void {
+    this.productsService.getProducts().subscribe({
+      next: (data)=>{
+        console.log('DATA >>>>', data);
+        this.products = data;
+        
+      },
+      error: (err)=>{
+        console.error(err);
+        
+      },
+      complete: ()=>{
+        console.log('Fetching data complete');
+        
+      }
+    })
+
+  }
+}
